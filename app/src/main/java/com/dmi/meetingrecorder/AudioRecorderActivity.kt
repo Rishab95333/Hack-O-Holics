a8ccbeb4b97c3efc2bad4990c8ffcc74988dffc4
Karan Singh <ksingh@dminc.com>
2018-02-23 14:28:18 +0530

    android tts integration

--- app/src/main/java/com/dmi/meetingrecorder/AudioRecorderActivity.kt
+++ app/src/main/java/com/dmi/meetingrecorder/AudioRecorderActivity.kt
@@ -7,9 +7,20 @@ import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.dmi.meetingrecorder.model.FirebaseMessageConversationObject
 import kotlinx.android.synthetic.main.activity_recorder.*
 import java.io.InputStream
+import net.gotev.speech.Speech
+import net.gotev.speech.GoogleVoiceTypingDisabledException
+import net.gotev.speech.SpeechRecognitionNotAvailable
+import net.gotev.speech.SpeechDelegate
+
+
+
+
 
 /**
  * Created by ajindal on 2/22/2018.
@@ -36,8 +47,11 @@ class AudioRecorderActivity : AppCompatActivity() {
         fab.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
                 recordAudio()
+                startVoiceToTextRecognition();
             }
         })
+
+        Speech.init(this, getPackageName());
     }
 
     private fun recordAudio() {
@@ -95,6 +109,85 @@ class AudioRecorderActivity : AppCompatActivity() {
             System.out.println("  # of features: " + mSimpleSpkDetection.featureCount())  // at this point, 0
             System.out.println("  # of models: " + mSimpleSpkDetection.speakerCount())
         }
+    }
+
+
+
+
+
+    /// speech to text
+    protected override fun onDestroy() {
+        super.onDestroy();
+        // prevent memory leaks when activity is destroyed
+        Speech.getInstance().shutdown()
+    }
+
+    lateinit var mRecognizedText: FirebaseMessageConversationObject;
+
+    protected fun startVoiceToTextRecognition(){
+        try {
+            // you must have android.permission.RECORD_AUDIO granted at this point
+            Speech.getInstance().startListening(object : SpeechDelegate {
+                override fun onStartOfSpeech() {
+                    Log.i("speech", "speech recognition is now active")
+                }
+
+                override fun onSpeechRmsChanged(value: Float) {
+                    Log.d("speech", "rms is now: " + value)
+                }
+
+                override fun onSpeechPartialResults(results: List<String>) {
+                    val str = StringBuilder()
+                    for (res in results) {
+                        str.append(res).append(" ")
+                    }
+
+                    Log.i("speech", "partial result: " + str.toString().trim { it <= ' ' })
+                }
+
+                override fun onSpeechResult(result: String) {
+                    if(result == null || result.length <= 0)
+                        return;
+                    Log.i("speech-result", "result: " + result)
+
+//                    var index1 = result.indexOf("Over");
+//                    var index2 = result.indexOf("over");
+//                    var index3 = result.indexOf("OVER");
+//
+//                    if(index1 >= 0 || index2 >= 0 || index3 >= 0){
+//                        var position=-1;
+//                        if(index1 >= 0){
+//                            position = index1;
+//                        }else if(index2 >= 0){
+//                            position = index2;
+//                        } else if(index3 >= 0){
+//                            position = index3;
+//                        }
+//                        if(position > 0) {
+//                            mRecognizedText.message = mRecognizedText.message + result.substring(0, position)
+//                            Toast.makeText(baseContext,mRecognizedText.message,Toast.LENGTH_SHORT)
+//                        }
+                    Speech.getInstance().shutdown();
+                    Speech.init(baseContext, getPackageName());
+                        startVoiceToTextRecognition();
+                        // save conversation on server
+
+                        // initialize speaker recognition again
+//                    }
+                }
+            })
+        } catch (exc: SpeechRecognitionNotAvailable) {
+            Log.e("speech", "Speech recognition is not available on this device!")
+            // You can prompt the user if he wants to install Google App to have
+            // speech recognition, and then you can simply call:
+            //
+            // SpeechUtil.redirectUserToGoogleAppOnPlayStore(this);
+            //
+            // to redirect the user to the Google App page on Play Store
+        } catch (exc: GoogleVoiceTypingDisabledException) {
+            Log.e("speech", "Google voice typing must be enabled!")
+        }
+
     }
 }
 
---
+++ app/src/main/java/com/dmi/meetingrecorder/model/FirebaseMessageConversationObject.kt
@@ -0,0 +1,13 @@
+package com.dmi.meetingrecorder.model
+
+/**
+ * Created by dmi on 15/02/18.
+ */
+public class FirebaseMessageConversationObject {
+
+    var speaker: String = "";
+    var message: String = "";
+
+
+
+}
